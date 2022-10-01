6f48e26f814f25a26ca2913c76b7cbe71b720419
alexraffy <92450474+alexraffy@users.noreply.github.com>
2022-10-01 20:19:19 +0200

    basic filter for DSJSONAdaptor

--- src/Datasource/DSJSONAdaptor.ts
+++ src/Datasource/DSJSONAdaptor.ts
@@ -12,15 +12,24 @@ export class DSJSONAdaptor implements DSAdaptor {
     private cursor_index0Based: number = 0;
     private cursor_currentRow: any = undefined;
     private cursor_eof: boolean = true;
-
-    constructor(data: any[]) {
+    private fn_filter: (o: any) => boolean;
+    constructor(data: any[], filter: (o: any) => boolean = undefined) {
         this.data = data;
+        this.fn_filter = filter;
     }
     link(dataSource: DataSource) {
         this.dataSource = dataSource;
     }
     getTotalRows(): number {
-        return (this.data !== undefined) ? this.data.length : 0;
+        if (this.data === undefined) { return 0; }
+        if (this.fn_filter === undefined) {
+            return this.data.length;
+        }
+        let counter = 0;
+        for (let i = 0; i < this.data.length; i++) {
+            if (this.fn_filter(this.data[i])) { counter++;}
+        }
+        return counter;
     }
     getObjectAtIndex(index0Based: number): any {
         this.cursor_currentRow = undefined;
@@ -29,8 +38,19 @@ export class DSJSONAdaptor implements DSAdaptor {
         if (index0Based < 0) { return undefined};
         if (index0Based >= this.getTotalRows()) { return undefined; }
         this.cursor_eof = false;
-        this.cursor_currentRow = this.data[index0Based];
-        return this.data[index0Based];
+        if (this.fn_filter === undefined) {
+            this.cursor_currentRow = this.data[index0Based];
+            return this.data[index0Based];
+        } else {
+            let counter = -1;
+            for (let i = 0; i < this.data.length; i++) {
+                if (this.fn_filter(this.data[i])) { counter++;}
+                if (counter === index0Based) {
+                    this.cursor_currentRow = this.data[index0Based];
+                    return this.data[index0Based];
+                }
+            }
+        }
     }
 
     getCurrentRow() {
@@ -57,13 +77,29 @@ export class DSJSONAdaptor implements DSAdaptor {
         return this.getObjectAtIndex(offset);
     }
     readNext(): any | undefined {
-        this.cursor_index0Based++;
-        if (this.dataSource.limit !== undefined && this.cursor_index0Based > this.dataSource.offset + this.dataSource.limit) {
-            this.cursor_currentRow = undefined;
-            this.cursor_eof = true;
-            return undefined;
+        if (this.fn_filter === undefined) {
+            this.cursor_index0Based++;
+            if (this.dataSource.limit !== undefined && this.cursor_index0Based > this.dataSource.offset + this.dataSource.limit) {
+                this.cursor_currentRow = undefined;
+                this.cursor_eof = true;
+                return undefined;
+            }
+            return this.getObjectAtIndex(this.cursor_index0Based);
+        } else {
+            while (1) {
+                this.cursor_index0Based++;
+                if (this.cursor_index0Based > this.data.length -1) {
+                    this.cursor_eof = true;
+                    this.cursor_currentRow = undefined;
+                    return undefined;
+                }
+                if (this.fn_filter(this.data[this.cursor_index0Based])) {
+                    this.cursor_currentRow = this.data[this.cursor_index0Based];
+                    this.cursor_eof = false;
+                    return this.cursor_currentRow;
+                }
+            }
         }
-        return this.getObjectAtIndex(this.cursor_index0Based);
     }
     eof(): boolean {
         return this.cursor_eof;
